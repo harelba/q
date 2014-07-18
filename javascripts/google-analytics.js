e25d01ca8cf174ed998245d2d10db433fcf07c55
Harel Ben-Attia <harelba@gmail.com>
2014-07-18 03:18:23 -0400

    Added download link tracking in GA

--- javascripts/google-analytics.js
+++ javascripts/google-analytics.js
@@ -5,6 +5,23 @@
 
   ga('create', 'UA-48316355-1', 'auto');
   ga('require', 'displayfeatures');
-  //ga('require', 'linkid', 'linkid.js');
+  ga('require', 'linkid', 'linkid.js');
   ga('send', 'pageview');
+
+// Monitor all download links in GA
+window.onload = function() {
+  var a = document.getElementsByTagName('a');
+  for (i = 0; i < a.length; i++) {
+      if (a[i].href.match(/^https?:\/\/.+(\.rpm|\/q|\.tar\.gz|\.zip)$/i)) {
+        a[i].onclick = function() {
+		  var that = this;
+          ga('send', 'event', 'Downloads', 'Click', this.getAttribute('href'));
+		  setTimeout(function() {
+			location.href = that.href;
+		  },500); 
+		  return false;
+        };
+      }
+  }
+}
 

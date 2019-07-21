e3d430d78405e79faacda4605ea1eab96a9e1de5
iliapolo <eli.polonsky@gmail.com>
2019-07-21 23:35:46 +0300

    Added url and description to setup.py

--- setup.py
+++ setup.py
@@ -19,9 +19,11 @@ from setuptools import setup
 
 setup(
     name='q',
+    url='https://github.com/harelba/q',
     license='LICENSE',
     version='1.7.4',
     author='Harel Ben-Attia',
+    description="Run SQL directly on CSV or TSV files",
     author_email='harelba@gmail.com',
     install_requires=[
         'six==1.11.0'

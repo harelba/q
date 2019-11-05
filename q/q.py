d16751d93ebc6b6fcc65c35548e4d11e3e3cd7ba
Harel Ben-Attia <harelba@gmail.com>
2019-11-05 14:33:21 +0200

    bump version

--- q/q.py
+++ q/q.py
@@ -31,7 +31,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-q_version = "1.7.4"
+q_version = "2.0.2"
 
 __all__ = [ 'QTextAsData' ]
 

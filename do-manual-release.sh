4d4634d6a44bc16f380b806829d2cee73662cbcd
Harel Ben-Attia <harelba@gmail.com>
2019-11-14 13:41:37 +0200

    wip

--- do-manual-release.sh
+++ do-manual-release.sh
@@ -12,7 +12,7 @@ if [ "x$TRAVIS_OS_NAME" == "xwindows" ]
 then
 	echo "Packing windows installer"
 	pyci pack nsis
-	pyci github upload-asset --asset q-$(uname -a)-$(uname -s)-installer.exe --release 2.0.2
+	pyci github upload-asset --asset q-AMD64-Windows.exe --release 2.0.2
 else
 	echo "Not in windows - not packing windows installer"
 fi

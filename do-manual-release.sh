b97e6fc01a42fbc7fbe0a20dcdf41e71ba98e332
Harel Ben-Attia <harelba@gmail.com>
2019-11-26 22:05:56 +0200

    wip

--- do-manual-release.sh
+++ do-manual-release.sh
@@ -10,10 +10,14 @@ then
 	pyci pack binary
 	pyci github upload-asset --asset q-$(uname -m)-$(uname -s) --release 2.0.2
 else
-	echo "Packing windows installer"
+	echo "Packing windows installer - packing binary"
 	pyci pack binary
-	find `pwd` -ls
+	echo "Packing windows installer - listing files"
+	find `pwd` -ls | grep -v \.git/
+	echo "Packing windows installer - packing nsis"
 	BINARY_LOCATION=`pwd`/q-AMD64-Windows.exe
 	pyci pack nsis --binary-path $BINARY_LOCATION --version 2.0.2.0
+	echo "Packing windows installer - uploading"
 	pyci github upload-asset --asset $BINARY_LOCATION --release 2.0.2
+	echo "done"
 fi

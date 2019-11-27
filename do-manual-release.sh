fe96d7384f8f15f43bf95c50dfe4252e3b159b19
Harel Ben-Attia <harelba@gmail.com>
2019-11-27 16:19:21 +0200

    wip

--- do-manual-release.sh
+++ do-manual-release.sh
@@ -15,7 +15,7 @@ else
 	echo "Packing windows installer - listing files"
 	find `pwd` -ls | grep -v \.git/
 	echo "Packing windows installer - packing nsis"
-	BINARY_LOCATION=`pwd`/q-AMD64-Windows.exe
+	BINARY_LOCATION="c:\\Users\\travis\\build\\harelba\\q\\q-AMD64-Windows.exe"
 	pyci pack nsis --binary-path $BINARY_LOCATION --version 2.0.2.0
 	echo "Packing windows installer - uploading"
 	pyci github upload-asset --asset $BINARY_LOCATION --release 2.0.2

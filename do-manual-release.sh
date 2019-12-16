001951a7b3a52e866d17d637391f870cc375dbe1
Harel Ben-Attia <harelba@gmail.com>
2019-12-16 20:06:35 +0200

    fix wierd hyphen bug

--- do-manual-release.sh
+++ do-manual-release.sh
@@ -19,7 +19,7 @@ else
 	find `pwd` -ls | grep -v \.git/
 	echo "Packing windows installer - packing nsis"
 	BINARY_LOCATION="c:\\Users\\travis\\build\\harelba\\q\\q-AMD64-Windows.exe"
-	pyci pack nsis -—program-files-dir q-TextAsData --binary-path $BINARY_LOCATION --version ${VERSION}.0
+	pyci pack nsis --program-files-dir q-TextAsData --binary-path $BINARY_LOCATION --version ${VERSION}.0
 	echo "Packing windows installer - uploading"
 	pyci github upload-asset --asset $BINARY_LOCATION --release $VERSION
 	SETUP_LOCATION="c:\\Users\\travis\\build\\harelba\\q\\q-AMD64-Windows-installer.exe"
